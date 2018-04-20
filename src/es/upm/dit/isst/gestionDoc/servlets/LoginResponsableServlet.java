779506dd5ed11611e040fa23fcae840f9a8aa5b9
isst <alemosan@hotmail.com>
2018-04-20 02:35:10 +0200

    Bug arreglado en LoginResponsable

--- src/es/upm/dit/isst/gestionDoc/servlets/LoginResponsableServlet.java
+++ src/es/upm/dit/isst/gestionDoc/servlets/LoginResponsableServlet.java
@@ -22,7 +22,7 @@ import es.upm.dit.isst.gestionDoc.dao.model.Profesor;
  */
 @WebServlet("/LoginResponsableServlet")
 public class LoginResponsableServlet extends HttpServlet {
-	
+
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		String menu = req.getParameter("menu");
@@ -31,15 +31,13 @@ public class LoginResponsableServlet extends HttpServlet {
 			req.getSession().setAttribute("menuResponsable", 0);
 			resp.sendRedirect(req.getContextPath() + "/LoginResponsable.jsp");
 		}else if(menu.equals("1")) {
-                       
-                       req.getSession().setAttribute("menuResponsable", 1);
-       
-                       resp.sendRedirect(req.getContextPath() + "/LoginResponsable.jsp");
-                       
-               	}
+
+			req.getSession().setAttribute("menuResponsable", 1);
+
+			resp.sendRedirect(req.getContextPath() + "/LoginResponsable.jsp");
 
 
-		
+
 			// Redireccionarte para editar las asignaturas
 		}else if(menu.equals("2")){
 			req.getSession().setAttribute("menuResponsable", 2);
@@ -49,7 +47,7 @@ public class LoginResponsableServlet extends HttpServlet {
 			List<Profesor> profesores = profesor.getDepartamento().getProfesores();
 			req.getSession().setAttribute("profesorDepartamento_list", profesores);
 			resp.sendRedirect(req.getContextPath() + "/LoginResponsable.jsp");
-	
+
 		}else if(menu.equals("3")){
 			req.getSession().setAttribute("menuResponsable", 3);
 			req.getSession().setAttribute("planEstudios_list", PlanEstudiosDAOImplementation.getInstance().readAll());
@@ -58,8 +56,8 @@ public class LoginResponsableServlet extends HttpServlet {
 			req.getSession().setAttribute("profesorDepartamento_list", profesores);
 			resp.sendRedirect(req.getContextPath() + "/LoginResponsable.jsp");
 		}
-		
-		
+
+
 	}
 
 }
